85af74460b58bd721ff57be123af6021357d7fdf
insamju400 <insamju400@gmail.com>
2024-08-28 20:50:15 +0900

    quiz2

---
+++ src/Level0/pcce2.java
@@ -0,0 +1,17 @@
+package Level0;
+
+import java.util.Scanner;
+//디버깅 문제. 이미 완성된 코드에서 버그 찾아 수정하기.
+//직각삼각형이 주어졌을 때 빗변의 제곱은 다른 두 변을 각각 제곱한 것의 합과 같습니다.
+//직각삼각형의 한 변의 길이를 나타내는 정수 a와 빗변의 길이를 나타내는 정수 c가 주어질 때, 다른 한 변의 길이의 제곱, b_square 을 출력하도록 한 줄을 수정해 코드를 완성해 주세요.
+public class pcce2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = c - a;
+
+        System.out.println(b_square);
+    }
+}
